5304476339480b14f00be386a36c0618be8a23e9
MimRe <madmadpv@gmail.com>
2025-11-26 22:31:20 +0330

    a little

--- d-s-lib/messages.h
+++ d-s-lib/messages.h
@@ -1,6 +1,9 @@
 const char LinkedListMenu[]{"\n\n1.linear_singly\n2.circular_singly\n3.linear_doubly\n4.circular_doubly\n0.exit\n>> "};
+
 const char AppMenu[]{"\n\n1.InsertAtBegin\n2.InsertInMid\n3.InsertAtEnd\n4.DelFromBegin\n5.DelFromMid\n6.DelFromEnd\n0.back\n>> "};
 
 const char GetIndex[]{"\nenter the index (int > 0)\n>> "};
 
 const char GetData[]{"\nenter the data (double)\n>> "};
+
+//MadMad_9
